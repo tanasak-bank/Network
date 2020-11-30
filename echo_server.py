ab31b8f15fcbada32c177eb70474889611fc4c5a
Anusorn Chaikaew <anusorn.c@crru.ac.th>
2020-11-30 09:25:57 +0700

    add new files to repository

---
+++ echo_server.py
@@ -0,0 +1,15 @@
+import socket
+import time
+with socket.socket() as s:
+    host = 'localhost'
+    port = 8001
+    s.bind((host, port))
+    print(f'socket binded to {port}')
+    s.listen()
+    con, addr = s.accept()
+    with con:
+        while True:
+            data = con.recv(1024)
+            if not data:
+                break
+            con.sendall(data)
